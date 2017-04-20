d79bfc766361ae756397b5c8f6bc89ae8b30f169
Benjamin <ayzenbergb@gmail.com>
2017-04-19 17:12:18 -0700

    file connection established

--- lab-ben/lib/reader.js
+++ lab-ben/lib/reader.js
@@ -1,3 +1,41 @@
 'use strict';
 
+module.exports = exports = {};
+
 const fs = require('fs');
+
+let retrevialOrder = [];
+
+exports.getSmall = function() {
+  fs.readFile(`${__dirname}/../text/small.txt`, function(err, data) {
+    if(err) throw err;
+    let smallData = data.toString();
+    console.log(smallData);
+    return smallData;
+  });
+};
+
+exports.getMedium = function() {
+  fs.readFile(`${__dirname}/../text/medium.txt`, function(err, data) {
+    if(err) throw err;
+    let mediumData = data.toString();
+    return mediumData;
+  });
+};
+
+exports.getLarge = function() {
+  fs.readFile(`${__dirname}/../text/large.txt`, function(err, data) {
+    if(err) throw err;
+    let largeData = data.toString();
+    return largeData;
+  });
+};
+
+exports.process = function(callback) {
+  Array.prototype.push.apply(retrevialOrder, [exports.getLarge(), exports.getSmall(), exports.getMedium()]);
+  callback(retrevialOrder);
+};
+
+exports.after = function(data) {
+  console.log(data);
+};
--- lab-ben/test/reader-test.js
+++ lab-ben/test/reader-test.js
@@ -3,3 +3,7 @@
 const fs = require('fs');
 const expect = require('chai').expect;
 const Buffer = require('buffer').Buffer;
+
+describe('reader.js', function() {
+  describe('#') 
+});
