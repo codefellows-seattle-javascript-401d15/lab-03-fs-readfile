a9c3bc56318e055ed17fece53a9dce2342be2026
Caleb <caleb.wells1@gmail.com>
2017-04-19 19:15:48 -0700

    tests work, code works

--- caleb-lab/lib/index.js
+++ caleb-lab/lib/index.js
@@ -6,7 +6,7 @@ fs.readFile(`${__dirname}/../data/text.txt`, function(err, lyric){
   if(err) throw err;
   console.log(Buffer(lyric).toString('utf-8', 0, 9));
   console.log(Buffer(lyric).toString('hex', 0, 9));
-
+  
   fs.readFile(`${__dirname}/../data/new-lyric.txt`, function(err, newLyric){
     if(err) throw err;
     console.log(Buffer(newLyric).toString('utf-8', 0, 9));
@@ -18,4 +18,5 @@ fs.readFile(`${__dirname}/../data/text.txt`, function(err, lyric){
     console.log(Buffer(newestLyric).toString('utf-8', 0, 9));
     console.log(Buffer(newestLyric).toString('hex', 0, 9));
   });
+
 });
