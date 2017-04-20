5863acec813942273875498612f3b43e5f7f812c
Caleb <caleb.wells1@gmail.com>
2017-04-20 11:38:45 -0700

    init

--- caleb-lab/lib/index.js
+++ caleb-lab/lib/index.js
@@ -2,9 +2,15 @@
 
 //require the filesystem
 const fs = require('fs');
+module.exports = exports = {};
+
+exports.firstString;
+exports.secondString;
+exports.thirdString;
 
 //read the file with the direct path
-fs.readFile(`${__dirname}/../data/text.txt`, function(err, lyric){
+exports.firstBuffer = fs.readFile(`${__dirname}/../data/text.txt`, function(err, lyric){
+  exports.firstString = Buffer(lyric).toString('hex', 0, 9);
   //if there's an error, stop everything and throw it
   if(err) throw err;
   //print filtered characters, offset by a number to include the full list
@@ -12,26 +18,32 @@ fs.readFile(`${__dirname}/../data/text.txt`, function(err, lyric){
   console.log(Buffer(lyric).toString('utf-8', 0, 9));
   // print the full thing, because Tupac
   console.log(Buffer(lyric).toString('utf-8'));
-
-  //read the file with the direct path, with a differentiation of the parameter naming
-  fs.readFile(`${__dirname}/../data/new-lyric.txt`, function(err, newLyric){
-    //if there's an error, stop everything and throw it
-    if(err) throw err;
-    //print filtered characters, offset by a number to include the full list
-    console.log(Buffer(newLyric).toString('hex', 0, 9));
-    console.log(Buffer(newLyric).toString('utf-8', 0, 9));
-    // print the full thing, because Tupac
-    console.log(Buffer(newLyric).toString('utf-8'));
-  });
-
-  //read the file with the direct path, with a differentiation of the parameter naming
-  fs.readFile(`${__dirname}/../data/newest-lyric.txt`, function(err, newestLyric){
-    //if there's an error, stop everything and throw it
-    if(err) throw err;
-    //print filtered characters, offset by a number to include the full list
-    console.log(Buffer(newestLyric).toString('hex', 0, 9));
-    console.log(Buffer(newestLyric).toString('utf-8', 0, 9));
-    // print the full thing, because Tupac
-    console.log(Buffer(newestLyric).toString('utf-8'));
-  });
 });
+
+//read the file with the direct path, with a differentiation of the parameter naming
+exports.secondBuffer = fs.readFile(`${__dirname}/../data/new-lyric.txt`, function(err, newLyric){
+  exports.secondString = Buffer(newLyric).toString('hex', 0, 9);
+  //if there's an error, stop everything and throw it
+  if(err) throw err;
+  //print filtered characters, offset by a number to include the full list
+  console.log(Buffer(newLyric).toString('hex', 0, 9));
+  console.log(Buffer(newLyric).toString('utf-8', 0, 9));
+  // print the full thing, because Tupac
+  console.log(Buffer(newLyric).toString('utf-8'));
+  // return exports.secondString;
+});
+
+//read the file with the direct path, with a differentiation of the parameter naming
+exports.thirdBuffer = fs.readFile(`${__dirname}/../data/newest-lyric.txt`, function(err, newestLyric){
+  exports.thirdBuffer.thirdString = Buffer(newestLyric).toString('hex', 0, 9);
+  //if there's an error, stop everything and throw it
+  if(err) throw err;
+  //print filtered characters, offset by a number to include the full list
+  console.log(Buffer(newestLyric).toString('hex', 0, 9));
+  console.log(Buffer(newestLyric).toString('utf-8', 0, 9));
+  // print the full thing, because Tupac
+  console.log(Buffer(newestLyric).toString('utf-8'));
+});
+
+
+console.log(exports.firstString + exports.secondString + exports.thirdString);
