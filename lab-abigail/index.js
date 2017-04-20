b6a91b5a5650e94a05098321af6d901e33f82626
Abigail White <abigailwhite213@gmail.com>
2017-04-19 17:10:13 -0700

    order text files

--- lab-abigail/index.js
+++ lab-abigail/index.js
@@ -2,23 +2,34 @@
 
 const fs = require('fs');
 
-fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
-  if(err) throw err;
-  let oneData = data.toString('hex', 0, 8);
-  console.log(oneData.toString());
-  return oneData;
-});
+module.exports = exports = {};
 
-fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
-  if(err) throw err;
-  let twoData = data.toString('hex', 0, 8);
-  console.log(twoData.toString());
-  return twoData;
-});
+exports.readText = function(callback) {
 
-fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
-  if(err) throw err;
-  let threeData = data.toString('hex', 0, 8);
-  console.log(threeData.toString());
-  return threeData;
-});
+  let text = [];
+  fs.readFile(`${__dirname}/data/one.txt`, function(err, data) {
+    if(err) throw err;
+    let oneData = data.toString('hex', 0, 8);
+    console.log(oneData);
+    text.push(oneData);
+
+    fs.readFile(`${__dirname}/data/two.txt`, function(err, data) {
+      if(err) throw err;
+      let twoData = data.toString('hex', 0, 8);
+      console.log(twoData);
+      text.push(twoData);
+
+      fs.readFile(`${__dirname}/data/three.txt`, function(err, data) {
+        if(err) throw err;
+        let threeData = data.toString('hex', 0, 8);
+        console.log(threeData);
+        text.push(threeData);
+
+        console.log(text);
+        callback(text);
+      });
+    });
+  });
+};
+
+exports.readText();
