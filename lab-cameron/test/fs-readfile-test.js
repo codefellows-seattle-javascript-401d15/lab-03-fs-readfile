9096b22347fa5ee2eb5fbb0fb5dff241e941d77f
Cameron Bacon <cameronmbacon@gmail.com>
2017-04-24 10:48:37 -0700

    trying to appease travis!

--- lab-cameron/test/fs-readfile-test.js
+++ lab-cameron/test/fs-readfile-test.js
@@ -8,20 +8,24 @@ let filePaths = [
   `${__dirname}/../data/second.txt`,
   `${__dirname}/../data/third.txt`,
 ];
-const hexBuffers = reader(filePaths);
 
+let hexBuffers = reader(filePaths);
 describe('fs-readfile.js', function() {
-  describe('#readFile', function() {
+  describe('#readFile(\'first.txt\')', function() {
     it('should read first.txt and return a hex string', function(done) {
       console.log('8 bytes of first.txt as a hex string', hexBuffers[0]);
       expect(typeof hexBuffers[0]).equal(typeof 'string');
       done();
     });
+  });
+  describe('#readFile(\'second.txt\')', function() {
     it('should read second.txt and return a hex string', function(done) {
       console.log('8 bytes of second.txt as a hex string', hexBuffers[1]);
       expect(typeof hexBuffers[1]).equal(typeof 'string');
       done();
     });
+  });
+  describe('#readFile(\'third.txt\')', function() {
     it('should read third.txt and return a hex string', function(done) {
       console.log('8 bytes of third.txt as a hex string', hexBuffers[2]);
       expect(typeof hexBuffers[2]).equal(typeof 'string');
